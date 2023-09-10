45b907220af557613824778eec38c9658d43ce5d
thomasbrian4545 <thomasbrian4545@gmail.com>
2023-09-10 23:58:28 +0700

    Mengakses view dari controller
    - Membuat array mahasiswa dan dikirim ke view mahasiswa

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers;
 
 class PageController extends Controller {
     public function index() {
-        return "Halaman Home";
+        // return "Halaman Home";
+        return view('welcome');
     }
 
     public function tampil() {
-        return "Data Mahasiswa";
+        // return "Data Mahasiswa";
+        $arrMahasiswa = ['Thomas Brian', 'Dewi Sartika Sadodolu', 'Camilla Ava Debriana'];
+        return view('mahasiswa')->with('mahasiswa', $arrMahasiswa);
     }
 }
