96bbae91ef68dd1b5e09bbf1eb59f53604ff704f
thomasbrian4545 <thomasbrian4545@gmail.com>
2023-09-11 23:57:03 +0700

    Return type : view

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -4,14 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\View\View;
 
 class ItemController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request): string
+    // public function __invoke(Request $request): string
+    public function __invoke(Request $request): View
     {
-        return "Method __invoke() milik ItemController dijalankan";
+        // return "Method __invoke() milik ItemController dijalankan";
+        $arrMahasiswa = [
+            "Risa Lestari", "Rudi Hermawan", "Bambang Kusumo",
+            "Lisa Permata"
+        ];
+
+        return view('mahasiswa')->with('mahasiswa', $arrMahasiswa);
     }
 }
